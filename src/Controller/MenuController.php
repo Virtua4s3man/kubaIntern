ca470a9ad1d2fbbd4a638679ffcf69d3532b3dc6
Jakub Kułaga <kuba.kulaga.sv7@gmail.com>
2019-02-15 15:00:09 +0100

    minor butify change in MenuController

--- src/Controller/MenuController.php
+++ src/Controller/MenuController.php
@@ -10,9 +10,8 @@ class MenuController extends AbstractController
 {
     public function index(ProductCategoryRepository $categoryRepository)
     {
-        $categories = $categoryRepository->findAll();
         return $this->render('menu/index.html.twig', [
-            'categories' => $categories,
+            'categories' => $categoryRepository->findAll(),
         ]);
     }
 }
