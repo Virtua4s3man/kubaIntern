ea8cc40c4b2259e21988b046f101f54eb643dcc3
kuba <intern4@wearevirtua.com>
2019-02-19 08:53:36 +0100

    random book upgrade

--- src/Controller/LibraryDivController.php
+++ src/Controller/LibraryDivController.php
@@ -18,11 +18,13 @@ class LibraryDivController extends AbstractController
      */
     public function index(AuthorRepository $authorRepo, BookRepository $bookRepo, GenreRepository $genreRepo)
     {
+        $bookAmount = $bookRepo->count([]);
+        $randomBook = $bookAmount ? $bookRepo->findBy([], null, 1, rand()%$bookAmount) : null;
         return $this->render('library_div/index.html.twig', [
             'authors' => $authorRepo->findBy([], null, 5),
             'books' => $bookRepo->findBy([], null, 5),
             'genres' => $genreRepo->findBy([], null, 5),
-            'random_book' => $bookRepo->findBy([], null, 1, rand()%$bookRepo->count([]))[0],
+            'random_book' => $randomBook,
         ]);
     }
 }
