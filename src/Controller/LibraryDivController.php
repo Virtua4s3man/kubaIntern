8ab546f58bec76b8141deaadf57179f31c849843
kuba <intern4@wearevirtua.com>
2019-02-19 09:30:41 +0100

    libdivcontroller embedded, random number made ok

--- src/Controller/LibraryDivController.php
+++ src/Controller/LibraryDivController.php
@@ -6,20 +6,13 @@ use App\Repository\AuthorRepository;
 use App\Repository\BookRepository;
 use App\Repository\GenreRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @Route("/library")
- */
 class LibraryDivController extends AbstractController
 {
-    /**
-     * @Route("/", name="library", methods={"GET"})
-     */
     public function index(AuthorRepository $authorRepo, BookRepository $bookRepo, GenreRepository $genreRepo)
     {
         $bookAmount = $bookRepo->count([]);
-        $randomBook = $bookAmount ? $bookRepo->findBy([], null, 1, rand()%$bookAmount) : null;
+        $randomBook = $bookAmount ? $bookRepo->findBy([], null, 1, rand()%$bookAmount)[0] : null;
         return $this->render('library_div/index.html.twig', [
             'authors' => $authorRepo->findBy([], null, 5),
             'books' => $bookRepo->findBy([], null, 5),
