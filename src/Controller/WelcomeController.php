cdb066b7d5d2d7d2fad223b85c8e55786bf83e68
kuba <intern4@wearevirtua.com>
2019-02-19 14:48:46 +0100

    initial wishlist exps

--- src/Controller/WelcomeController.php
+++ src/Controller/WelcomeController.php
@@ -3,6 +3,9 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Session\Attribute\NamespacedAttributeBag;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Session\Storage\NativeSessionStorage;
 use Symfony\Component\Routing\Annotation\Route;
 
 class WelcomeController extends AbstractController
@@ -12,6 +15,12 @@ class WelcomeController extends AbstractController
      */
     public function index()
     {
+        $session = new Session(new NativeSessionStorage(), new NamespacedAttributeBag());
+        $session->set('wishlist/prod1', 'ok');
+        $session->set('user/dono', 'user');
+
+        dump($session);
+
         return $this->render('welcome/index.html.twig', [
             'controller_name' => 'Welcome',
             'date' => new \DateTime(),
