ab08e23db5fe08ea3138d58baf7feee3fcc31418
kuba <intern4@wearevirtua.com>
2019-02-20 10:05:21 +0100

    before moving wishlist logic to productController

--- src/Controller/WishlistController.php
+++ src/Controller/WishlistController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\Attribute\NamespacedAttributeBag;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpFoundation\Session\Storage\NativeSessionStorage;
@@ -14,29 +15,31 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class WishlistController extends AbstractController
 {
-    private $sessionNamespace = 'wishlist/';
+    private $sessionNamespace = 'wishlist';
     private $redirectionRoute = 'product_index';
 
     /**
      * @Route("/{id}", name="wishlist_add", methods={"POST"})
      */
-    public function wishListAdd(Product $product)
+    public function wishListAdd(Request $request, Product $product)
     {
-        $id = $product->getId();
-        $session = new Session(new NativeSessionStorage(), new NamespacedAttributeBag());
-        $session->set($this->namespaceName($id), $id);
+        if ($this->isCsrfTokenValid('add'.$product->getId(), $request->request->get('_token'))) {
+            $this->addToWishlist($product);
+        }
 
-        $this->redirectToRoute($this->redirectionRoute);
+        return $this->redirectToRoute($this->redirectionRoute);
     }
 
     /**
      * @Route("/{id}", name="wishlist_remove", methods={"DELETE"})
      */
-    public function wishListRemove(Product $product)
+    public function wishListRemove(Request $request, Product $product)
     {
         $id = $product->getId();
-        $session = new Session(new NativeSessionStorage(), new NamespacedAttributeBag());
-        $session->remove($this->namespaceName($id));
+        if ($this->isCsrfTokenValid('add'.$product->getId(), $request->request->get('_token'))) {
+            $session = new Session(new NativeSessionStorage(), new NamespacedAttributeBag());
+            $session->remove($this->namespaceName($id));
+        }
 
         $this->redirectToRoute($this->redirectionRoute);
     }
@@ -49,8 +52,31 @@ class WishlistController extends AbstractController
 
     }
 
+    private function addToWishlist(Product $product)
+    {
+        $id = $product->getId();
+        $session = new Session(new NativeSessionStorage(), new NamespacedAttributeBag());
+        if (5 > $this->countWishlistProducts($session)) {
+            $session->set($this->namespaceName($id), $id);
+            $this->addFlash('success', $product->getName() . ' added to wishlist');
+        } else {
+            $this->addFlash('warning', 'wishlist can\'t contain more than 5 products');
+        }
+    }
+
+    private function countWishlistProducts(Session $session)
+    {
+        return count($this->getWishlist($session));
+    }
+
+    private function getWishlist(Session $session): array
+    {
+        $wishlist = $session->get($this->sessionNamespace);
+        return $wishlist ? $wishlist : [];
+    }
+
     private function namespaceName($name): string
     {
-        return $this->sessionNamespace . $name;
+        return $this->sessionNamespace . '/'. $name;
     }
 }
