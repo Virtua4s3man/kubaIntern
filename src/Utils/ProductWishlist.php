273fa470e748925381f4236e66084021c22617b3
kuba <intern4@wearevirtua.com>
2019-02-21 10:15:49 +0100

    utils/productWishlist added

---
+++ src/Utils/ProductWishlist.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: virtua
+ * Date: 2019-02-21
+ * Time: 10:14
+ */
+
+namespace App\Utils;
+
+
+class ProductWishlist
+{
+    CONST WISHLIST_PREFIX = 'wishlist_product';
+
+}
